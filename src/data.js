f8bdd2a8c625ae0a831f5b59fe0e75b1312c7e30
Seethala Jyothi Swarup <85427735+swarupe7@users.noreply.github.com>
2023-07-11 16:25:48 +0530

    Update data.js

--- src/data.js
+++ src/data.js
@@ -20,12 +20,6 @@ export const products = [
       link: "https://utube-seven.vercel.app",
     },{
     id:4,
-        img:pdfX,
-        link:"https://pdf-x-1zmy.onrender.com",
-    
-    
-    },{
-    id:5,
         img:ylite,
         link:"https://rococo-gecko-4700df.netlify.app",
     
